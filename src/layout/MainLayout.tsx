6303a73793001bc63cbfd49d990d745fb614908e
tundeLayan <tunderoxyafolayan@gmail.com>
2022-01-23 02:13:52 +0100

    main layout

---
+++ src/layout/MainLayout.tsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { RootStateType } from "../redux";
+
+const MainLayout = ({ children }: any) => {
+  const {
+    users: { query },
+  } = useSelector((state: RootStateType) => state);
+  const [value, setValue] = useState(query);
+  useEffect(() => {
+    setValue(query);
+  }, [query]);
+  return (
+    <div>
+      <div>
+        <nav className="header">
+          <ul className="nav-lists">
+            <li>Logo</li>
+            {!!query ? (
+              <li>
+                <input
+                  onChange={(e) => setValue(e.target.value)}
+                  value={value}
+                />
+              </li>
+            ) : null}
+            <li>Pull requests</li>
+            <li>Issues</li>
+            <li>Marketplace</li>
+            <li>Explore</li>
+          </ul>
+          <ul className="list-actions">
+            <li>Bell</li>
+            <li>Plus</li>
+            <li>Profile</li>
+          </ul>
+        </nav>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default MainLayout;
